5419b126fed5594bde601bd6d02875d80f5583b2
EpicEmu <epicemu@epicemu.com>
2016-02-14 18:45:43 -0800

    Pet Damage Fixes
    Pets hit for way too much in newer version of database, let's tone it
    down to where it belongs.

---
+++ pet_fixes/pet_dmg_fix.sql
@@ -0,0 +1,195 @@
+#written by faerie shadov
+#brought to you by skorge
+#materials found at http://samanna.net/eq.general/pets.shtml#nec
+#enjoy!
+
+#skel_pet_1_
+UPDATE npc_types SET hp = 30 WHERE id = 614;
+UPDATE npc_types SET maxdmg = 10 WHERE id = 614;
+#skel_pet_5_
+UPDATE npc_types SET hp = 90 WHERE id = 615;
+UPDATE npc_types SET maxdmg = 12 WHERE id = 615;
+#skel_pet_9_
+UPDATE npc_types SET hp = 200 WHERE id = 616;
+UPDATE npc_types SET maxdmg = 14 WHERE id = 616;
+#skel_pet_11_
+UPDATE npc_types SET hp = 200 WHERE id = 617;
+UPDATE npc_types SET maxdmg = 16 WHERE id = 617;
+#skel_pet_16_
+UPDATE npc_types SET hp = 300 WHERE id = 618;
+UPDATE npc_types SET maxdmg = 20 WHERE id = 618;
+#skel_pet_19_
+UPDATE npc_types SET hp = 375 WHERE id = 619;
+UPDATE npc_types SET maxdmg = 22 WHERE id = 619;
+#skel_pet_22_
+UPDATE npc_types SET hp = 700 WHERE id = 620;
+UPDATE npc_types SET maxdmg = 26 WHERE id = 620;
+#skel_pet_25_
+UPDATE npc_types SET hp = 775 WHERE id = 621;
+UPDATE npc_types SET maxdmg = 28 WHERE id = 621;
+#skel_pet_29_
+UPDATE npc_types SET hp = 850 WHERE id = 622;
+UPDATE npc_types SET maxdmg = 32 WHERE id = 622;
+#skel_pet_33_
+UPDATE npc_types SET hp = 1000 WHERE id = 623;
+UPDATE npc_types SET maxdmg = 39 WHERE id = 623;
+#skel_pet_37_
+UPDATE npc_types SET hp = 1300 WHERE id = 624;
+UPDATE npc_types SET maxdmg = 47 WHERE id = 624;
+#skel_pet_41_
+UPDATE npc_types SET hp = 2300 WHERE id = 625;
+UPDATE npc_types SET maxdmg = 55 WHERE id = 625;
+#skel_pet_43_
+UPDATE npc_types SET hp = 1400 WHERE id = 626;
+UPDATE npc_types SET maxdmg = 57 WHERE id = 626;
+#skel_pet_44_
+UPDATE npc_types SET hp = 1700 WHERE id = 627;
+UPDATE npc_types SET maxdmg = 57 WHERE id = 627;
+#skel_pet_47_
+UPDATE npc_types SET hp = 2700 WHERE id = 628;
+UPDATE npc_types SET maxdmg = 69 WHERE id = 628;
+#skel_pet_61_
+UPDATE npc_types SET hp = 3300 WHERE id = 629;
+UPDATE npc_types SET maxdmg = 73 WHERE id = 629;
+#skel_pet_63_
+UPDATE npc_types SET hp = 2500 WHERE id = 630;
+UPDATE npc_types SET maxdmg = 73 WHERE id = 630;
+#skel_pet_65_
+UPDATE npc_types SET hp = 4200 WHERE id = 631;
+UPDATE npc_types SET maxdmg = 78 WHERE id = 631;
+#skel_pet_67_
+UPDATE npc_types SET hp = 6000 WHERE id = 632;
+UPDATE npc_types SET maxdmg = 90 WHERE id = 632;
+#skel_pet_70_
+UPDATE npc_types SET hp = 4000 WHERE id = 633;
+UPDATE npc_types SET maxdmg = 100 WHERE id = 633;
+
+
+## MAGE PETS
+
+#WATER
+#SumWateR2
+UPDATE npc_types SET hp = 72, maxdmg = 12 WHERE id = 599;
+#SumWateR3
+UPDATE npc_types SET hp = 195, maxdmg = 16 WHERE id = 600;
+#SumWateR4
+UPDATE npc_types SET hp = 275, maxdmg = 18 WHERE id = 601;
+#SumWateR5
+UPDATE npc_types SET hp = 415, maxdmg = 20 WHERE id = 602;
+#SumWateR6
+UPDATE npc_types SET hp = 540, maxdmg = 23 WHERE id = 603;
+#SumWateR7
+UPDATE npc_types SET hp = 665, maxdmg = 26 WHERE id = 604;
+#SumWateR8
+UPDATE npc_types SET hp = 800, maxdmg = 28 WHERE id = 605;
+#SumWateR9
+UPDATE npc_types SET hp = 920, maxdmg = 34 WHERE id = 606;
+#SumWateR10
+UPDATE npc_types SET hp = 1170, maxdmg = 40 WHERE id = 607;
+#SumWateR11
+UPDATE npc_types SET hp = 1350, maxdmg = 48 WHERE id = 608;
+#SumWateR12
+UPDATE npc_types SET hp = 2520, maxdmg = 56 WHERE id = 609;
+#SumWateR13
+UPDATE npc_types SET hp = 2350, maxdmg = 56 WHERE id = 610;
+#SumWateR14
+UPDATE npc_types SET hp = 2500, maxdmg = 68 WHERE id = 611;
+
+#FIRE
+#SumFire2
+UPDATE npc_types SET hp = 47, maxdmg = 12 WHERE id = 582;
+#SumFire3
+UPDATE npc_types SET hp = 128, maxdmg = 16 WHERE id = 583;
+#SumFire4
+UPDATE npc_types SET hp = 182, maxdmg = 18 WHERE id = 584;
+#SumFire5
+UPDATE npc_types SET hp = 275, maxdmg = 20 WHERE id = 585;
+#SumFire6
+UPDATE npc_types SET hp = 350, maxdmg = 23 WHERE id = 586;
+#SumFire7
+UPDATE npc_types SET hp = 415, maxdmg = 26 WHERE id = 587;
+#SumFire8
+UPDATE npc_types SET hp = 510, maxdmg = 28 WHERE id = 588;
+#SumFire9
+UPDATE npc_types SET hp = 575, maxdmg = 34 WHERE id = 589;
+#SumFire10
+UPDATE npc_types SET hp = 760, maxdmg = 40 WHERE id = 590;
+#SumFire11
+UPDATE npc_types SET hp = 880, maxdmg = 48 WHERE id = 591;
+#SumFire12
+UPDATE npc_types SET hp = 1650, maxdmg = 56 WHERE id = 592;
+#SumFire13
+UPDATE npc_types SET hp = 1750, maxdmg = 36 WHERE id = 593;
+#SumFire14
+UPDATE npc_types SET hp = 1850, maxdmg = 48 WHERE id = 594;
+
+#AIR
+#SumAir2
+UPDATE npc_types SET hp = 58, maxdmg = 12 WHERE id = 552;
+#SumAir3
+UPDATE npc_types SET hp = 160, maxdmg = 16 WHERE id = 553;
+#SumAir4
+UPDATE npc_types SET hp = 225, maxdmg = 18 WHERE id = 554;
+#SumAir5
+UPDATE npc_types SET hp = 345, maxdmg = 20 WHERE id = 555;
+#SumAir6
+UPDATE npc_types SET hp = 445, maxdmg = 23 WHERE id = 556;
+#SumAir7
+UPDATE npc_types SET hp = 540, maxdmg = 26 WHERE id = 557;
+#SumAir8
+UPDATE npc_types SET hp = 700, maxdmg = 28 WHERE id = 558;
+#SumAir9
+UPDATE npc_types SET hp = 800, maxdmg = 34 WHERE id = 559;
+#SumAir10
+UPDATE npc_types SET hp = 960, maxdmg = 40 WHERE id = 560;
+#SumAir11
+UPDATE npc_types SET hp = 1125, maxdmg = 48 WHERE id = 561;
+#SumAir12
+UPDATE npc_types SET hp = 2205, maxdmg = 56 WHERE id = 562;
+#SumAir13
+UPDATE npc_types SET hp = 2650, maxdmg = 56 WHERE id = 563;
+#SumAir14
+UPDATE npc_types SET hp = 2650, maxdmg = 68 WHERE id = 564;
+
+#EARTH
+#SumEarth2
+UPDATE npc_types SET hp = 89, maxdmg = 10 WHERE id = 567;
+#SumEarth3
+UPDATE npc_types SET hp = 240, maxdmg = 12 WHERE id = 568;
+#SumEarth4
+UPDATE npc_types SET hp = 340, maxdmg = 14 WHERE id = 569;
+#SumEarth5
+UPDATE npc_types SET hp = 515, maxdmg = 16 WHERE id = 570;
+#SumEarth6
+UPDATE npc_types SET hp = 665, maxdmg = 18 WHERE id = 571;
+#SumEarth7
+UPDATE npc_types SET hp = 820, maxdmg = 23 WHERE id = 572;
+#SumEarth8
+UPDATE npc_types SET hp = 1000, maxdmg = 25 WHERE id = 573;
+#SumEarth9
+UPDATE npc_types SET hp = 1150, maxdmg = 28 WHERE id = 574;
+#SumEarth10
+UPDATE npc_types SET hp = 1450, maxdmg = 32 WHERE id = 575;
+#SumEarth11
+UPDATE npc_types SET hp = 1675, maxdmg = 36 WHERE id = 576;
+#SumEarth12
+UPDATE npc_types SET hp = 3075, maxdmg = 42 WHERE id = 577;
+#SumEarth13
+UPDATE npc_types SET hp = 3150, maxdmg = 48 WHERE id = 578;
+#SumEarth14
+UPDATE npc_types SET hp = 3200, maxdmg = 48 WHERE id = 579;
+
+#MONSTER
+#MonsterSum1
+UPDATE npc_types SET hp = 1000, maxdmg = 28, mindmg = 8 WHERE id = 541;
+#MonsterSum2
+UPDATE npc_types SET hp = 1450, maxdmg = 40, mindmg = 16 WHERE id = 542;
+#MonsterSum3
+UPDATE npc_types SET hp = 3075, maxdmg = 48, mindmg = 18 WHERE id = 543;
+#MonsterSum4
+UPDATE npc_types SET hp = 3200, maxdmg = 48, mindmg = 24 WHERE id = 544;
+
+
+
+
+
